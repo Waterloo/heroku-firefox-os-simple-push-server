3a4b1d2fe0f69ac6e6829e0f63111e286110ff6e
riddler <cvdrops@outlook.com>
2015-11-15 18:32:10 +0530

    Add Welcome Page

--- server/server.js
+++ server/server.js
@@ -1,50 +1,60 @@
 var express = require('express'),
     http = require('http'),
-    simplePush = require('../simplepush/client.js');
+    simplePush = require('../simplepush/client.js'),
+    bodyParser = require('body-parser');
+
 
 var Server = function Server() {
 
-  var app = express();
-  var pushClient = simplePush;
+    var app = express();
 
-  var configure = function configure() {
+    var pushClient = simplePush;
 
-    app.use(express.bodyParser());
-    app.use(function(req, res, next) {
-      res.contentType('application/json');
-      next();
-    });
+    var configure = function configure() {
 
-    var port = process.env.PORT || 8080;
-    var mongoURL = process.env.MONGOURL || 'mongodb://127.0.0.1:27017/simplepush';
-    // For running in appfog
-    if (process.env.VCAP_SERVICES) {
-      var services = JSON.parse(process.env.VCAP_SERVICES);
-      mongoURL = services['mongodb-1.8'][0].credentials.url;
-    }
+        app.use(bodyParser.json()); // to support JSON-encoded bodies
+        app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+            extended: false
+        }));
+        app.use(express.static(__dirname + '/public'));
+//        app.get('/',function (req,res){});
+        
+        app.use(function (req, res, next) {
+            res.contentType('application/json');
+            next();
+        });
 
-    app.set('port', port);
-    app.set('mongoURL', mongoURL);
+        var port = process.env.PORT || 8080;
+        var mongoURL = process.env.MONGOURL || 'mongodb://127.0.0.1:27017/simplepush';
+        // For running in appfog
+        if (process.env.VCAP_SERVICES) {
+            var services = JSON.parse(process.env.VCAP_SERVICES);
+            mongoURL = services['mongodb-1.8'][0].credentials.url;
+        }
 
-    pushClient.init(mongoURL);
+        app.set('port', port);
+        app.set('mongoURL', mongoURL);
 
-    app.post('/api/v1/register', pushClient.register);
-    app.post('/api/v1/unregister', pushClient.unregister);
-    app.post('/api/v1/', pushClient.send);
-    app.get('/api/v1/:seq', pushClient.get);
+        pushClient.init(mongoURL);
 
-  };
+        app.post('/api/v1/register', pushClient.register);
+        app.post('/api/v1/unregister', pushClient.unregister);
+        app.post('/api/v1/', pushClient.send);
+        app.get('/api/v1/:seq', pushClient.get);
 
-  var start = function start() {
-    configure();
-    http.createServer(app).listen(app.get('port'), function() {
-      console.log('Express server listening on port ' + app.get('port'));
-    });
-  };
+    };
 
-  return {
-    'start': start
-  };
+    var start = function start() {
+        configure();
+        http.createServer(app).listen(app.get('port'), function () {
+            console.log('Express server listening on port ' + app.get('port'));
+            console.log(__dirname);
+        });
+    };
+
+    return {
+        'start': start
+    };
 
 }();
 
